026f811ef8e71f943473c611b19d43f0a6c78f32
Joseff361 <Joseff361@outlook.com>
2022-04-04 21:07:00 -0500

    Make scaffold

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { store, key } from './store';
 
-createApp(App).use(store).mount('#app')
+import '@/styles/styles.scss';
+
+createApp(App).use(store, key).mount('#app');
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,24 @@
-import { createStore } from 'vuex'
+import { InjectionKey } from 'vue';
+import { createStore, useStore as baseUseStore, Store } from 'vuex';
 
-export default createStore({
+export interface State {
+  album: unknown;
+  tracks: unknown;
+}
+
+export const key: InjectionKey<Store<State>> = Symbol();
+
+export const store = createStore<State>({
   state: {
+    album: null,
+    tracks: null,
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
+
+export function useStore() {
+  return baseUseStore(key);
+}
